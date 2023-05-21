05399f4dbf295ba65de9bf252f01c3db5d13bcb3
plsnotjoe <96671050+plsnotjoe@users.noreply.github.com>
2023-05-21 15:35:12 +0100

    added todo bits

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -14,10 +14,10 @@ func main() {
 	i, _ := m.Generate()
 	// with Makefile remove ../../
 	f, err := os.Create("../../internal/output/img.jpg")
-
 	if err != nil {
 		log.Fatalln(err)
 	}
+	
 	err = jpeg.Encode(f, i, nil)
 	if err != nil {
 		log.Fatalln(err)
